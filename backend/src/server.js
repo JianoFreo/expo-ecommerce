9357a6a5ce8cfa7829473a831003f81932b6fb5f
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-05-08 03:20:12 +0800

    tetsing and identifieing the envs and ports the server is running form inside the server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,15 +15,14 @@ if (ENV.NODE_ENV === "production") {
   // express.static() is middleware that tells Express:
   // “Serve files from this folder directly to the browser.”
 
-  
+
   app.get("/{*any}", (req, res) => { // if its on any routes exept api routes
-    res.sendFile(path.join(__dirname, "../admin/dist", "index.html")); 
+    res.sendFile(path.join(__dirname, "../admin/dist", "index.html"));
     //path.join is used to join the path of the dist folder and index.html file
     //Instead of:
     //__dirname + "/admin/dist"
     //we use:
     //path.join(__dirname, "admin", "dist")
-
 
     //sendFile() is an Express method used to send an actual file to the browser.
   });
@@ -31,4 +30,6 @@ if (ENV.NODE_ENV === "production") {
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+  console.log("PORT:", process.env.PORT);
 });
