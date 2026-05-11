628ec38ded73bdb38c1866cd779bedaaca9f447f
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-05-11 21:43:38 +0800

    Add comment to clarify order status update route

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -22,7 +22,7 @@ router.get("/products", getAllProducts);
 router.put("/products/:id",upload.array("images", 3), updateProduct);
 
 router.get("/orders", getAllOrders);
-router.patch("/orders/:orderId/status", updateOrderStatus);
+router.patch("/orders/:orderId/status", updateOrderStatus); //pending -> shipped -> delivered
 
 
 export default router;
