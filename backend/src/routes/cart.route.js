b46bce9399f49c7c67fd2af46437f068c16cce92
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-05-18 00:16:10 +0800

    feat: implement cart controller and routes for managing cart operations

--- backend/src/routes/cart.route.js
+++ backend/src/routes/cart.route.js
@@ -1,7 +1,22 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+    getCart,
+    addToCart,
+    updateCart,
+    removeFromCart,
+    clearCart
+} from "../controllers/cart.controller.js";
 
 const router = Router();
+
 router.use(protectRoute) // middleware
 
-export default router;
+router.get("/", getCart)
+router.put("/", addToCart)
+router.put("/:productId", updateCart)
+router.delete("/:productId", removeFromCart)
+router.delete("/", clearCart)
+
+
+export default router; 
