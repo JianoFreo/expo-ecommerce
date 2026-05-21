24906269c5cc680b53361ae47465bc92db9e7189
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-05-21 11:20:51 +0800

    refactor(sso-callback): remove unused search parameters and clean up effect dependencies

--- mobile/app/--/sso-callback.tsx
+++ mobile/app/--/sso-callback.tsx
@@ -1,16 +1,15 @@
 import { useEffect } from "react";
-import { useRouter, useSearchParams } from "expo-router";
+import { useRouter } from "expo-router";
 import { View, ActivityIndicator } from "react-native";
 
 export default function SsoCallback() {
   const router = useRouter();
-  const params = useSearchParams();
 
   useEffect(() => {
     // After Clerk/SSO redirects here, send the user to root which will fetch /users/me
     // and route them into admin or tabs depending on their role.
     router.replace("/");
-  }, [params]);
+  }, []);
 
   return (
     <View className="flex-1 items-center justify-center bg-background">
